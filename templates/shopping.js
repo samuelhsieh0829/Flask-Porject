3fcd7b0035d2684fcb6cd1e85db3c7b08f435487
Xiang <danny2970519@gmail.com>
2024-06-12 17:48:31 +0800

    星巴克點餐系統70%

---
+++ templates/shopping.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#chocolate').click(function() {
+        window.location.href = 'order.html';
+    });
+
+    $('#greentea').click(function() {
+        window.location.href = 'order.html';
+    });
+
+    $('#caramel').click(function() {
+        window.location.href = 'order.html';
+    });
+
+    $('#back').click(function() {
+        window.location.href = 'welcome_user.html';
+    });
+});
